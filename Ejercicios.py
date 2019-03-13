a1854e71f0257fd122c7898180cd86abf98a458c
Juan Carlos Galvis Rodriguez <jcgalvis6@hotmail.com>
2019-03-12 20:02:27 -0500

    Ejercicio 9

--- Ejercicios.py
+++ Ejercicios.py
@@ -92,6 +92,68 @@ def esDobleDeUnImpar(numero):
         raise TypeError(str(numero) + ' no es entero')
     else:
         impar = numero / 2
+        
+ def desgloseMinimoBilletes(valor):
+    '''
+    num -> str
+
+    >>> desgloseMinimoBilletes(434)
+    2 billetes de 200 euros.
+    1 billetes de 20 euros.
+    1 billetes de 10 euros.
+    2 monedas de 2 euros.
+
+    :param valor:
+    :return:
+    '''
+
+    mensaje = ''
+
+    if (valor // 500) != 0:
+        mensaje += str((valor // 500)) + ' billetes de 500 euros.'
+        mensaje += '\n'
+        valor = valor - (valor // 500) * 500
+
+    if (valor // 200) != 0:
+        mensaje += str((valor // 200)) + ' billetes de 200 euros.'
+        mensaje += '\n'
+        valor = valor - (valor // 200) * 200
+
+    if (valor // 100) != 0:
+        mensaje += str((valor // 100)) + ' billetes de 100 euros.'
+        mensaje += '\n'
+        valor = valor - (valor // 100) * 100
+
+    if (valor // 50) != 0:
+        mensaje += str((valor // 50)) + ' billetes de 50 euros.'
+        mensaje += '\n'
+        valor = valor - (valor // 50) * 50
+
+    if (valor // 20) != 0:
+        mensaje += str((valor // 20)) + ' billetes de 20 euros.'
+        mensaje += '\n'
+        valor = valor - (valor // 20) * 20
+
+    if (valor // 10) != 0:
+        mensaje += str((valor // 10)) + ' billetes de 10 euros.'
+        mensaje += '\n'
+        valor = valor - (valor // 10) * 10
+
+    if (valor // 5) != 0:
+        mensaje += str((valor // 5)) + ' billetes de 5 euros.'
+        mensaje += '\n'
+        valor = valor - (valor // 5) * 5
+
+    if (valor // 2) != 0:
+        mensaje += str((valor // 2)) + ' monedas de 2 euros.'
+        mensaje += '\n'
+        valor = valor - (valor // 2) * 2
+
+    if (valor // 1) != 0:
+        mensaje += str((valor // 1)) + ' monedas de 1 euros.'
+        valor = valor - (valor // 1) * 1
+
+    print('%s' % mensaje.rstrip('\n'))
 
         if (impar % 2) != 0:
             return str(numero) + ' es doble de ' + str(int(impar)) + ', que es impar'
